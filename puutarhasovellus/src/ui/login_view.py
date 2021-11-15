88791dd6a35551abf0eca631a3211b8ef16211e8
Jan Rundt <jan.rundt@gmail.com>
2021-11-15 17:17:48 +0200

    working on login

--- puutarhasovellus/src/ui/login_view.py
+++ puutarhasovellus/src/ui/login_view.py
@@ -2,7 +2,19 @@ from tkinter import ttk, StringVar, constants
 from services.gardening_service import gardening_service
 
 class LoginView:
+    '''An object that creates the loginview.
+    '''
+
     def __init__(self, root, show_mainview, show_registration, show_adminview):
+        '''The constructor.
+
+        Args:
+            root: the tkinter-root-object.
+            show_mainview: a function to show the mainview if login is successful
+            show_registration: a function to swith to the registration window
+            show_adminview: a function to show the adminview is login is succesful for an admin
+        '''
+
         self._root = root
         self._show_mainview = show_mainview
         self._show_registration = show_registration
@@ -17,3 +29,8 @@ class LoginView:
 
     def destroy(self):
         self._frame.destroy()
+
+    def _handle_login(self):
+        username = self._user_entry.get()
+        password = self._pw_entry.get()
+        print(f"Looks like {username} has password {password}")
