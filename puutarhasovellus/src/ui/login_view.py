d9de05d3f2b9b33ab7bce2fd01dc2e354b63889e
Jan Rundt <jan.rundt@gmail.com>
2021-11-15 17:05:25 +0200

    working on login

--- puutarhasovellus/src/ui/login_view.py
+++ puutarhasovellus/src/ui/login_view.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, StringVar, constans
+from tkinter import ttk, StringVar, constants
 from services.gardening_service import gardening_service
 
 class LoginView:
@@ -7,4 +7,13 @@ class LoginView:
         self._show_mainview = show_mainview
         self._show_registration = show_registration
         self._show_adminview = show_adminview
+        self._frame = None
+        self._user_entry = None
+        self._pw_entry = None
         #continue filling up all variables and then self.initialize()
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
