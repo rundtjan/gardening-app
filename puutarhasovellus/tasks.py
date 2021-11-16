35d1cb2d88893f1dff7602f0a457e1e88fa83ed6
Jan Rundt <jan.rundt@gmail.com>
2021-11-16 13:57:39 +0200

    tasks

--- puutarhasovellus/tasks.py
+++ puutarhasovellus/tasks.py
@@ -1,5 +1,17 @@
 from invoke import task
 
 @task
+def build(ctx):
+    ctx.run("mkdir data; py src/db_init.py")
+
+@task
 def start(ctx):
     ctx.run("py src/index.py")
+
+@task
+def alt_build(ctx):
+    ctx.run("mkdir data; python3 src/db_init.py")
+
+@task
+def alt_start(ctx):
+    ctx.run("python3 src/index.py")
